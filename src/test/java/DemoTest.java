a1fa39a684c53d9471c73a7c844b766966be2b71
santhosh shetty <santhoshshetty58@gmail.com>
2024-05-31 16:33:39 +0200

    added the test to run on ios

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,6 +1,9 @@
+import java.util.Map;
 import com.mobile.driverhandler.DriverFactory;
 import com.mobile.driverhandler.Server;
 import io.appium.java_client.AppiumBy;
+import lombok.SneakyThrows;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.annotations.Test;
 
 public class DemoTest {
@@ -12,7 +15,18 @@ public class DemoTest {
   }
 
   @Test
+  @SneakyThrows
   public void demoIOSTest() {
     DriverFactory.getDriverInstance("iOSSimulator").setup();
+    Server.getDriver().findElement(AppiumBy.accessibilityId("Drag")).click();
+    Thread.sleep(5000);
+    RemoteWebElement source = (RemoteWebElement) Server.getDriver().findElement(AppiumBy.accessibilityId("drag-l2"));
+    RemoteWebElement target = (RemoteWebElement) Server.getDriver().findElement(AppiumBy.accessibilityId("drop-l2"));
+    dragAndDrop(source, target);
+  }
+
+  public static void dragAndDrop(RemoteWebElement source, RemoteWebElement destination) throws InterruptedException {
+    Server.getDriver().executeScript("gesture: dragAndDrop",
+        Map.of("sourceId", source.getId(), "destinationId", destination.getId()));
   }
 }
